df52a80d72af666b824bdb9465f72124f3e9ff4f
Rami <gerassi.rami@gmail.com>
2018-01-29 22:28:57 +0200

    a

--- CodeGym/src/main/java/com/rami/gerassi/gym/TestHello.java
+++ CodeGym/src/main/java/com/rami/gerassi/gym/TestHello.java
@@ -2,10 +2,15 @@ package com.rami.gerassi.gym;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 
 @Component
 public class TestHello {
     public TestHello() {
         System.out.printf("AAAAAA");
     }
+
+    @PostConstruct
+    void a(){System.out.printf("BBBBB");}
 }
